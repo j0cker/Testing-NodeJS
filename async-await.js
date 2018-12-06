1d81b56a6c18b902e7feb5c63ceda2c01bbbe630
Emiliano <emiliano.teran@getnewt.com>
2018-12-05 19:33:48 -0600

    await

--- async-await.js
+++ async-await.js
@@ -19,3 +19,21 @@ getNombre().then(nombre => {
 }).catch(e => {
     console.log("Error de ASYNC", e);
 })
+
+/* para evitar toda la definicion anterior usamos await */
+
+let saludo = async() => {
+
+    /* si async nunca retornara ningún valor, await se quedaría escuchando para la eternidad*/
+
+    let nombre = await getNombre();
+
+    return "Hola " + nombre;
+
+}
+
+saludo().then(mensaje => {
+    console.log(mensaje);
+}).catch(e => {
+    console.log("Error de ASYNC", e);
+})
